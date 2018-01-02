9d7fe01481c7095d3f6bcd95dc7874ed82d8e530
khalid.cuet <khalid.cuet@gmail.com>
2018-01-01 23:47:48 -0500

    2nd commit

--- CareerCloud.ADODataAccessLayer/CompanyProfileRepository.cs
+++ CareerCloud.ADODataAccessLayer/CompanyProfileRepository.cs
@@ -1,12 +1,151 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
 
 namespace CareerCloud.ADODataAccessLayer
 {
-    class CompanyProfileRepository
+    class CompanyProfileRepository : BaseClass,IDataRepository<CompanyProfilePoco>
     {
+        public void Add(params CompanyProfilePoco[] items)
+        {
+            using (SqlConnection conn = new SqlConnection(_connString))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                foreach (CompanyProfilePoco poco in items)
+                {
+                    cmd.CommandText = @"INSERT INTO [dbo].[Company_Profiles]
+                                           ([Id]
+                                           ,[Registration_Date]
+                                           ,[Company_Website]
+                                           ,[Contact_Phone]
+                                           ,[Contact_Name]
+                                           ,[Company_Logo])
+                                     VALUES
+                                           (@Id
+                                           ,@Registration_Date
+                                           ,@Company_Website
+                                           ,@Contact_Phone
+                                           ,@Contact_Name
+                                           ,@Company_Logo)";
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+
+
+                }
+
+            }
+
+        }
+
+        public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<CompanyProfilePoco> GetAll(params Expression<Func<CompanyProfilePoco, object>>[] navigationProperties)
+        {
+            using (SqlConnection conn = new SqlConnection(_connString))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = @"SELECT [Id]
+                                  ,[Registration_Date]
+                                  ,[Company_Website]
+                                  ,[Contact_Phone]
+                                  ,[Contact_Name]
+                                  ,[Company_Logo]
+                                  ,[Time_Stamp]
+                              FROM [dbo].[Company_Profiles]";
+                int counter = 0;
+                CompanyProfilePoco[] pocos = new CompanyProfilePoco[500];
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    CompanyProfilePoco poco = new CompanyProfilePoco();
+                    poco.Id = rdr.GetGuid(0);
+                    poco.RegistrationDate = rdr.GetDateTime(1);
+                    poco.CompanyWebsite = rdr.GetString(2);
+                    poco.ContactPhone = rdr.GetString(3);
+                    poco.ContactName = rdr.GetString(4);
+                    poco.CompanyLogo = (byte[])rdr[5];
+                    poco.TimeStamp = (byte[])rdr[6];
+
+                    pocos[counter] = poco;
+                    counter++;
+                }
+                return pocos.Where(a => a != null).ToList();
+            }
+        }
+
+        public IList<CompanyProfilePoco> GetList(Func<CompanyProfilePoco, bool> where, params Expression<Func<CompanyProfilePoco, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CompanyProfilePoco GetSingle(Func<CompanyProfilePoco, bool> where, params Expression<Func<CompanyProfilePoco, object>>[] navigationProperties)
+        {
+            CompanyProfilePoco[] pocos = GetAll().ToArray();
+            return pocos.Where(where).ToList().FirstOrDefault();
+        }
+
+        public void Remove(params CompanyProfilePoco[] items)
+        {
+            using (SqlConnection conn = new SqlConnection(_connString))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                foreach (var poco in items)
+                {
+                    cmd.CommandText = @"DELETE FROM [dbo].[Company_Profiles]
+                                 WHERE Id=@Id";
+                    cmd.Parameters.AddWithValue("@Id", poco.Id);
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+
+
+                }
+
+
+            }
+        }
+
+        public void Update(params CompanyProfilePoco[] items)
+        {
+            using (SqlConnection conn = new SqlConnection(_connString))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                foreach (var poco in items)
+                {
+                    cmd.CommandText = @"UPDATE [dbo].[Company_Profiles]
+                                   SET [Id] = @Id
+                                      ,[Registration_Date] = @RegistrationDate
+                                      ,[Company_Website] = @CompanyWebsite
+                                      ,[Contact_Phone] = @ContactPhone
+                                      ,[Contact_Name] = @ContactName
+                                      ,[Company_Logo] = @CompanyLogo
+                                          WHERE Id=@Id";
+                    cmd.Parameters.AddWithValue("@Id", poco.Id);
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+
+                }
+
+            }
+
+
+        }
+
     }
 }
+
---
+++ CareerCloud.ADODataAccessLayer/SystemCountryCodeRepository.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+
+namespace CareerCloud.ADODataAccessLayer
+{
+    class SystemCountryCodeRepository : BaseClass,IDataRepository<SystemCountryCodePoco>
+    {
+        public void Add(params SystemCountryCodePoco[] items)
+        {
+            using (SqlConnection conn = new SqlConnection(_connString))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                foreach (SystemCountryCodePoco poco in items)
+                {
+                    cmd.CommandText = @"INSERT INTO [dbo].[System_Country_Codes]
+                                           ([Code]
+                                           ,[Name])
+                                     VALUES
+                                           (@Code
+                                           ,@Name)";
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+
+
+                }
+
+            }
+
+        }
+
+        public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<SystemCountryCodePoco> GetAll(params Expression<Func<SystemCountryCodePoco, object>>[] navigationProperties)
+        {
+            using (SqlConnection conn = new SqlConnection(_connString))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = @"SELECT [Code]
+                                          ,[Name]
+                                      FROM [dbo].[System_Country_Codes]";
+                int counter = 0;
+                SystemCountryCodePoco[] pocos = new SystemCountryCodePoco[500];
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    SystemCountryCodePoco poco = new SystemCountryCodePoco();
+                    poco.Code = rdr.GetString(0);
+                    poco.Name = rdr.GetString(1);
+                    pocos[counter] = poco;
+                    counter++;
+                }
+                return pocos.Where(a => a != null).ToList();
+            }
+        }
+
+        public IList<SystemCountryCodePoco> GetList(Func<SystemCountryCodePoco, bool> where, params Expression<Func<SystemCountryCodePoco, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SystemCountryCodePoco GetSingle(Func<SystemCountryCodePoco, bool> where, params Expression<Func<SystemCountryCodePoco, object>>[] navigationProperties)
+        {
+            SystemCountryCodePoco[] pocos = GetAll().ToArray();
+            return pocos.Where(where).ToList().FirstOrDefault();
+        }
+
+        public void Remove(params SystemCountryCodePoco[] items)
+        {
+            using (SqlConnection conn = new SqlConnection(_connString))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                foreach (var poco in items)
+                {
+                    cmd.CommandText = @" DELETE FROM [dbo].[System_Country_Codes]
+                                      WHERE <Search Conditions,,>
+                                 WHERE Code=@Code";
+                    cmd.Parameters.AddWithValue("@Code", poco.Code);
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+
+
+                }
+
+
+            }
+        }
+
+        public void Update(params SystemCountryCodePoco[] items)
+        {
+            using (SqlConnection conn = new SqlConnection(_connString))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                foreach (var poco in items)
+                {
+                    cmd.CommandText = @"UPDATE [dbo].[System_Country_Codes]
+                                           SET [Code] = @Code
+                                              ,[Name] = @Name
+                                          WHERE Code=@Code";
+                    cmd.Parameters.AddWithValue("@Code", poco.Code);
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+
+                }
+
+            }
+
+
+        }
+
+    }
+}
+
